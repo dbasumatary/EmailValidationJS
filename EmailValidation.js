2b11f517f933aee9488af68480a3a93b391bf0be
dbasumatary <basudhondip@gmail.com>
2023-04-01 00:16:15 +0530

    Added: Validating email with valid special characters

--- EmailValidation.js
+++ EmailValidation.js
@@ -1,6 +1,6 @@
-//UC3 - Email has to have "." symbol after email server name
+//UC4 - Handling optional part with valid special characters
 
-const emailRegex = RegExp("^(?=.{3,20}@)[a-zA-Z]{2,}[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-_.][a-zA-Z0-9-]+(\\.[A-Za-z0-9-]+)$");
+const emailRegex = RegExp("^(?=.{3,20}@)[a-zA-Z]+[A-Za-z0-9]+[- + . _]?[a-zA-Z0-9]+.@[^-_.][a-zA-Z0-9-]+(\\.[A-Za-z]+)(\\.[a-zA-Z]+)*$");
 
 function validateEmail(email){
     if(emailRegex.test(email))
@@ -10,8 +10,8 @@ function validateEmail(email){
 }
 
 try{
-    validateEmail("rahul@ggg.com");   //valid
-    validateEmail("rahulkkk@dfgd");  //invalid
+    validateEmail("rahul_jha@ggg.co.in");   //valid
+    validateEmail("rahul$kkk@dfgd.com");  //invalid
 }catch(e){
     console.error(e);
 }
